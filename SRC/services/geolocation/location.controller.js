6c5718bbbff3bd3aedfac64df3a69b4cf3d4a8a7
Ian Flores <ianflores07@pop-os.localdomain>
2023-06-20 21:16:26 -0500

    location controller updated

--- SRC/services/geolocation/location.controller.js
+++ SRC/services/geolocation/location.controller.js
@@ -2,11 +2,10 @@ import Establishment from "../../establishment/Establishment";
 import findNearestEstablishment from "./calculate";
 import places from "./mockDtaLocation";
 export async function getNearestPlaces(req,res,next){
+    const {lat, lon} = req.body;
     try{
-        const latitudReferencia = 9.0557154343145426;
-        const longitudReferencia = -79.5083552054962;
         const establishments = await Establishment.find({}).exec()
-        const nearestEstablishments = findNearestEstablishment({lat : latitudReferencia, lon: longitudReferencia, establishments: establishments});
+        const nearestEstablishments = findNearestEstablishment({lat : lat, lon: lon, establishments: establishments});
         res.status(200).json({data: nearestEstablishments})
     }catch(e){
         next(e)
