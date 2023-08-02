5f50f780bb776022d5cd5fa34c2b6e918b3963d0
codeboy0407 <ian.flores2@utp.ac.pa>
2023-08-02 13:55:57 -0500

    product routes updated

--- SRC/products/product.router.js
+++ SRC/products/product.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 const router = Router();
-import { create, createMany, getProduct, getSingleProduct } from "./product.controller";
+import { create, createMany, getProduct, getProductByName, getProductById } from "./product.controller";
 import { userValidation } from "../utils/auth";
 router.post("/create/one", create)
+router.post("/get/by/name", getProductByName)
+router.post("/get/by/id", getProductById)
 router.post("/get/many", getProduct)
-router.post("/get/one", getSingleProduct)
 router.get("/create/many", userValidation, createMany)
 
 export default router
