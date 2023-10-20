96a533fa3839601dfe295c245b8df47756f55c05
codeboy0407 <ian.flores2@utp.ac.pa>
2023-10-19 19:06:18 -0500

    roles added, other keys added

--- SRC/config/dev.js
+++ SRC/config/dev.js
@@ -1,11 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 export const config = {
-  ACCESS_TOKEN_SECRET:
-    "82e5c2fdbd4174d7c51b63ceee67e9cd3fd1cf48463f99ca69ac31df664f54560321075bc796749d47861881896cb67c4b2285681c854ca8f4cc573a34e87c93",
-  userDB:
-    "mongodb+srv://draigon07:Draigon2004@cluster0.85xa1ve.mongodb.net/Users?retryWrites=true&w=majority",
-  familiesDB:
-    "mongodb+srv://draigon07:Draigon2004@cluster0.85xa1ve.mongodb.net/Families?retryWrites=true&w=majority",
-  productsDB:
-    "mongodb+srv://draigon07:Draigon2004@cluster0.85xa1ve.mongodb.net/Products?retryWrites=true&w=majority",
-  sessionDB: "mongodb://localhost:27017/sessions",
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  DBURI: process.env.DB_URI,
+  port: process.env.PORT,
+  userDB: process.env.USERDB,
+  productsDB: process.env.PRODUCTDB,
+  sessionDB: process.env.SESSIONDB,
+  roles: {
+    READ: process.env.READ,
+    ADMIN: process.env.ADMIN
+  }
 };
