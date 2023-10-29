4d810ebe9089c91a20fd7a8c4714b92a6ea17f52
codeboy0407 <ian.flores2@utp.ac.pa>
2023-10-28 19:14:34 -0500

    connection updated

--- SRC/utils/connection.js
+++ SRC/utils/connection.js
@@ -1,17 +1,15 @@
 import mongoose from "mongoose";
 import { MongoClient, ServerApiVersion } from "mongodb";
-const dotenv = require("dotenv");
-dotenv.config({ path: "config.env" });
-
+import { config } from "../config/dev";
 
 async function makeConnection() {
   try {
-     await mongoose.connect(process.env.DB_URI, {
+    mongoose.connect(config.DBURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     console.log("Connected correctly to server");
-  }catch(e){
+  } catch (e) {
     console.error(e)
   }
 }
